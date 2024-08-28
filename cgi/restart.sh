f2158e23f188df1131f44a3e9de3f95d243e52f2
Jan Zwiegers <jan@radicalsystems.co.za>
2024-08-28 11:09:31 +0200

    Update reboot script

--- cgi/restart.sh
+++ cgi/restart.sh
@@ -6,15 +6,10 @@
 # (C) 2016-2022, Radical Electronic Systems - www.radicalsystems.co.za
 # Written by Jan Zwiegers, jan@radicalsystems.co.za
 
+echo -e "Access-Control-Allow-Origin: *\r\nContent-Type: application/json\r\n\r\n"
 
-echo -e "Content-Type: application/json\r\n\r\n"
-
-if [ "$REQUEST_METHOD" = "POST" ]; then
- #   sudo /sbin/reboot
-    echo -e "{\"status\":\"OK\"}"
-    /sbin/reboot
-else
-    echo -e "{\"status\":\"FAILED\", \"message\":\"not a post\"}"
-fi
+#   sudo /sbin/reboot
+echo -e "{\"status\":\"OK\"}"
+sudo /sbin/reboot
 
 
